8228ea7e9316ae6b6f3d778d4bdfd223599b85f2
Tikhomirov Ruslan <tikhomir.rs@gmail.com>
2026-03-26 15:48:24 +0500

    Task4

--- Task4/author-book-relationship/routes/web.php
+++ Task4/author-book-relationship/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/{user}', function (App\Models\User $user) {
+    return $user -> name;
+});
